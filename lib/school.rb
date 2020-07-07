ed86418072ed8731cb3c753e6caed43c64d40f65
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-07 02:57:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,4 @@
-class School
+lass School
   attr_accessor :name, :roster
   
   def initialize(name)
@@ -7,31 +7,15 @@ class School
   end 
   
   def add_student(student, level)
-   
+  
     roster[level] ||= []
     roster[level] << student
   end 
   
-  
- def grade(level)
+  def grade(level)
     roster.detect do |x, y| 
       if x == level
         return y 
       end 
     end 
   end 
-end
-
-
-school = School.new("Bayside High School")
-school.roster
-school.add_student("Zach Morris", 9)
-school.add_student("Moshe Zeus", 9)
-school.add_student("Onkelos Octavius", 9)
-school.roster
-school.add_student("Sugar Zaza", 5)
-school.roster
-school.add_student("Theo Madus", 5)
-school.add_student("Abbah Zabbah", 2)
-school.roster
-school.grade(9)
